75c5924aee0477dcb0ed2d83af4f4df29e02244f
kaiyuanluo <88017193+kaiyuanluo@users.noreply.github.com>
2024-07-13 11:36:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import random
+import art
+from game_data import data
+from replit import clear
+score = 0
+not_continue = True
+# print(art.logo)
+while not_continue:
+    print(art.logo)
+    _ = random.randint(0,len(data)-1)
+    new_list = data.pop(_)
+    a = data[_]['follower_count']
+    
+    print(f"Compare A: {data[_]['name']}, a {data[_]['description']}, from {data[_]['country']}.\n{art.vs}")
+    
+    _ = random.randint(0,len(data)-1)
+    new_list = data.pop(_)
+    b = data[_]['follower_count']
+    
+    print(f"Against B: {data[_]['name']}, a {data[_]['description']}, from {data[_]['country']}.")
+    
+    choice = input('who has more followers? type \'A\' or \'B\' ').lower()
+    
+    if a > b and choice == 'a':
+        score += 1
+        clear()
+        print(f"you are right! score = {score}.")
+    elif b > a and choice == 'b':
+        score += 1
+        clear()
+        print(f"you are right! score = {score}.")
+    else:
+        not_continue = Falsea
